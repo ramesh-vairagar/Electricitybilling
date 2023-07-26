eb3e26a9c95d5a85e788d21d05f6460b4f2f020d
Ramesh Vairagar <ramesh.vairagar11@gmail.com>
2023-07-26 23:10:46 +0530

    initial commit

---
+++ Electricity-Billing-System-master/Electricity-Billing-System/src/electricity/billing/system/Conn.java
@@ -0,0 +1,21 @@
+package electricity.billing.system;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs","root","1234");
+            s = c.createStatement();
+  
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+		new Conn();
+	}
+}
